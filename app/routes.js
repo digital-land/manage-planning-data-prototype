401b31b0e4f3d46614588141e5f62bb2b69185fa
Adam Silver <adambsilver@gmail.com>
2024-01-11 14:52:14 +0000

    Flesh out scaffolding

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,12 @@
-//
-// For guidance on how to create routes see:
-// https://prototype-kit.service.gov.uk/docs/create-routes
-//
-
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+
+router.all('*', (req, res, next) => {
+  res.locals.query = req.query
+  next()
+})
+
+require('./routes/account')(router)
+require('./routes/datasets')(router)
+
